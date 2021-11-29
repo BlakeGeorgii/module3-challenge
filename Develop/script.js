dc3d65e120f512cc3e070492fca4d1b22359f6b6
Blake Georgii <blakegeorgii@gmail.com>
2021-11-29 00:00:23 -0800

    created avaliableChars and changed user criteria variables into if statments filling out avaliableChars

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,29 @@
 // Assignment code here
 var generatePassword = function(){
   var pass = "test";
-
   var passLength = window.prompt("Insert length of password.");
+  var availableChars = "";
 
-  var isLower = window.confirm("Would you like your password to have Lower Case characters?");
-
-  var isUpper = window.confirm("Would you like your password to have Upper Case characters?");
-
-  var isNumeric = window.confirm("Would you like your password to have Numeric characters?");
+  //Adds lowercase characters to available characters if user agrees
+  if(window.confirm("Would you like your password to have Lower Case characters?")){
+    availableChars += "abcdefghijklmnopqrstuvwxyz";
+  }
+  //Adds uppercase characters to available characters if user agrees
+  if(window.confirm("Would you like your password to have Upper Case characters?")){
+    availableChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
   
-  var isSpecial = window.confirm("Would you like your password to have Special characters?");
+  if(window.confirm("Would you like your password to have Numeric characters?")){
+    availableChars += "0123456789";
+  }
   
+  if(window.confirm("Would you like your password to have Special characters?")){
+    availableChars += "\ \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~";
+  }
+
   return pass;
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
